7e53a0fb11ac4b06b048e4161e6f505a62cb4f98
unknown <elmoumen202a@gmail.com>
2024-03-04 01:57:26 +0100

    tests4

--- airline0/flights/models.py
+++ airline0/flights/models.py
@@ -18,7 +18,7 @@ class Flight(models.Model):
         return f"{self.id} : {self.origin} to {self.destination}"
 
     def is_valid_flight(self):
-        return self.origin != self.destination or self.duration >= 0  # to and
+        return self.origin != self.destination and self.duration >= 0  # to and
 
 
 class Passenger(models.Model):
